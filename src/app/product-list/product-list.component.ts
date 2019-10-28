e2ae8420fd35bd72c29063d1e91aa16c90525bcd
rbocazl <rbocazl@gmail.com>
2019-10-27 23:37:01 -0300

    4 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import { product } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  product = product;
+  products = product;
 
   share() {
     window.alert('The product has been shared!');
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,4 +1,4 @@
-export const product = [
+export const products = [
   {
     name: 'Phone XL',
     price: 799,
