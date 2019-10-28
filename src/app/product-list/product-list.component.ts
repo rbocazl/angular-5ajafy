d678550d1843d8436e150a287263eacc493f6019
rbocazl <rbocazl@gmail.com>
2019-10-27 23:05:44 -0300

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { product } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -8,7 +8,7 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+  product = product;
 
   share() {
     window.alert('The product has been shared!');
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,4 +1,4 @@
-export const products = [
+export const product = [
   {
     name: 'Phone XL',
     price: 799,
